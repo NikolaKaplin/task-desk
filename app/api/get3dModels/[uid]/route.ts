cf11a7760a2b38f29943efd9c0ae70d9b373a6dd
NikolaKaplin <kaplinkola0@gmail.com>
2025-02-19 23:45:31 +0300

    fixed server api error "cache"

--- app/api/get3dModels/[uid]/route.ts
+++ app/api/get3dModels/[uid]/route.ts
@@ -4,12 +4,27 @@ export async function GET(
   req: NextRequest,
   { params }: { params: Promise<{ uid: string }> }
 ) {
-  const { uid } = await params;
-  const url = `https://webapp.engineeringlumalabs.com/api/v3/creations/uuid/${uid}`;
-  const options = {
-    method: "GET",
-  };
-  const response = await fetch(url, options);
-  const data = await response.json();
-  return NextResponse.json(data);
+  try {
+    const { uid } = await params;
+    const url = `https://webapp.engineeringlumalabs.com/api/v3/creations/uuid/${uid}`;
+    console.log(`Fetching data for UID: ${uid}`);
+    const response = await fetch(url, {
+      method: "GET",
+      cache: "no-store",
+    });
+    const data = await response.json();
+    console.log(`Received data:`, data);
+    return NextResponse.json(data);
+  } catch (err) {
+    const { uid } = await params;
+    const url = `https://webapp.engineeringlumalabs.com/api/v3/creations/uuid/${uid}`;
+    console.log(`Fetching data for UID: ${uid}`);
+    const response = await fetch(url, {
+      method: "GET",
+      cache: "no-store",
+    });
+    const data = await response.json();
+    console.log(`Received data:`, data);
+    return NextResponse.json(data);
+  }
 }
